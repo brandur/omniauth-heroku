5f8d19d58d8cb161ce02ca2fb8ab5d3baef880b8
Brandur <brandur@mutelight.org>
2013-03-01 16:02:26 -0800

    Associate a negotiation with a Request-ID

--- lib/omniauth/strategies/heroku.rb
+++ lib/omniauth/strategies/heroku.rb
@@ -1,4 +1,5 @@
 require 'omniauth-oauth2'
+require 'securerandom'
 
 module OmniAuth
   module Strategies
@@ -6,10 +7,19 @@ module OmniAuth
       BaseAuthUrl = ENV["HEROKU_AUTH_URL"] || "https://api.heroku.com"
 
       option :client_options, {
-        :site => BaseAuthUrl,
         :authorize_url => "#{BaseAuthUrl}/oauth/authorize",
+        :site => BaseAuthUrl,
         :token_url => "#{BaseAuthUrl}/oauth/token"
       }
+
+      def request_phase
+        request_id = SecureRandom.uuid
+        options[:connection_opts] = { :headers => {
+          "Request-ID" => request_id
+        } }
+        log :info, "Request-ID for OAuth negotiation: #{request_id}"
+        super
+      end
     end
   end
 end
